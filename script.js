1c8c34e370ac6a5bae3a5a353f7cb2ae80008cb3
Naresh207 <107973553+Naresh207@users.noreply.github.com>
2023-11-17 13:10:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+// You can add JavaScript functionality here if needed
+// For example, handling click events or performing additional actions
+// This example doesn't require specific JavaScript functionality
