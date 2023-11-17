781f90976d89251dc9e4b6dcfea26ec0e841260d
naresh_venkate_official <107973553+Naresh207@users.noreply.github.com>
2023-11-18 01:01:12 +0530

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-// You can add JavaScript functionality here if needed
-// For example, handling click events or performing additional actions
-// This example doesn't require specific JavaScript functionality
